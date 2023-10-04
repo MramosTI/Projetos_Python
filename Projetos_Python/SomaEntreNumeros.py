85fe2ea14ce108a15b97386d3b5c55064bcfa866
Matheus Ramos <mramostidev@gmail.com>
2023-10-04 15:09:56 -0300

    Atualização

---
+++ Projetos_Python/SomaEntreNumeros.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+soma = numero1 + numero2
+print('A soma entre {} + {} = {}'.format(numero1, numero2, soma))
+
+
+
